5e706e66aad43013f30a16d8acbed187e98d3044
Ojesanmi <ojesanmiaa@nih.gov>
2023-01-06 10:43:55 -0500

    fix: modified deploy.sh script to add secret from aws

--- deploy/chp-api/deploy.sh
+++ deploy/chp-api/deploy.sh
@@ -23,13 +23,10 @@ do
 done
 
 sed -i.bak \
-    -e "s/DB_SECRET_KEY_VALUE/$DB_SECRET_KEY/g;s/APP_DJANGOALLOWEDHOSTS_VALUE/$APP_DJANGOALLOWEDHOSTS/g" \
-    -e "s/DB_ENGINE_VALUE/$DB_ENGINE/g;s/DB_DATABASE_VALUE/$DB_DATABASE/g" \
+    -e "s/DB_SECRET_KEY_VALUE/$DB_SECRET_KEY/g;s/DB_DATABASE_VALUE/$DB_DATABASE/g" \
     -e "s/DB_USERNAME_VALUE/$DB_USERNAME/g;s/DB_PASSWORD_VALUE/$DB_PASSWORD/g" \
-    -e "s/DB_HOST_VALUE/$DB_HOST/g;s/DB_PORT_VALUE/$DB_PORT/g" \
-    -e "s/DB_TYPE_VALUE/$DB_TYPE/g" \
     values-ncats.yaml
-rm values-ncats.yaml.bak 
+rm values-ncats.yaml
 
 kubectl apply -f namespace.yaml
 
